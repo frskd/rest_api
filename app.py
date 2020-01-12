2092e715285a5470ed6a35482c3bcdc71323348b
Miikis <Miikis@users.noreply.github.com>
2020-01-12 02:41:21 -0800

    Implement GET /locations
    Signed-off-by: Miikis <Miikis@users.noreply.github.com>

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
+from flask import Flask
+from flask_restful import Resource, Api
+
 from models import USAState, Ethnicity, StopType, data_frame_from_input, outcomes_for_data_frame
 
-inputs = data_frame_from_input(
-    USAState("arizona"), 12, 16, Ethnicity("black"), StopType(1))
-outcomes = outcomes_for_data_frame(inputs)
-print(outcomes)
+app = Flask(__name__)
+api = Api(app)
 
-# GET /locations Return a list of supported USA states: id, state_name
+
+class Locations(Resource):
+    def get(self):
+        """
+        Return a list of supported USA states: Array<{id: str, name: str}>
+        """
+        return [{"id": state.name, "name": state.value} for state in USAState]
 
 # GET /ethnicities Returns a list of supported ethnicities:
 # white, hispanic, not_listed, black, asian/pacific islander, unspecified
@@ -18,3 +25,12 @@ print(outcomes)
 # age: int - Between 10 and 110. (Defaults to 16)
 # is_pedestrian_stop: bool - Vehicular or pedestrian stop. (Defaults to false)
 # ethnicityId: int - Ethnicity Id. (Default to unspecified)
+
+
+api.add_resource(Locations, "/locations")
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    inputs = data_frame_from_input(
+        USAState("arizona"), 12, 16, Ethnicity("black"), StopType(1))
+    outcomes = outcomes_for_data_frame(inputs)
