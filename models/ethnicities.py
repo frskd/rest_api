29ebb4e9e13bc8491458c946fe1263d57c509ff7
Miikis <Miikis@users.noreply.github.com>
2020-01-12 02:07:04 -0800

    Add enum of supported ethnicities
    Signed-off-by: Miikis <Miikis@users.noreply.github.com>

---
+++ models/ethnicities.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+
+class Ethnicity(Enum):
+    caucasian = "white"
+    hispanic = "hispanic"
+    african_american = "black"
+    asian = "asian/pacific islander"
+    pacific_islander = "asian/pacific islander"
+    other = "other/unknown"
+    not_specfied = "unknown"
