3abe5a8f4d75a08860ce1979856bd28a990a9030
Miikis <Miikis@users.noreply.github.com>
2020-01-15 02:31:01 -0800

    Enable CORS
    Signed-off-by: Miikis <Miikis@users.noreply.github.com>

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_restful import Resource, Api
+from flask_cors import CORS
 
 from resources import Locations, Ethnicities, OutcomesList, Outcomes
 
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 
 
