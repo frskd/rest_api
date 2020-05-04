d0aa5c3afb06e1b4ec750be20b63b96a376d87a2
Miikis <Miikis@users.noreply.github.com>
2020-05-03 20:01:41 -0700

    Setup .env loading
    Signed-off-by: Miikis <Miikis@users.noreply.github.com>

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
+import os
+
 from flask import Flask
+from dotenv import load_dotenv
 from flask_restful import Resource, Api
 from flask_cors import CORS
 
 from resources import Locations, Ethnicities, OutcomesList, Outcomes
+
+load_dotenv()
 
 
 app = Flask(__name__)
@@ -16,4 +21,6 @@ api.add_resource(OutcomesList, "/outcomes")
 api.add_resource(Outcomes, "/outcomes/<location>")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    debug = os.getenv("DEBUG") == "true"
+    port = int(os.getenv("PORT")) if os.getenv("PORT") else 5000
+    app.run(debug=debug, port=port)
